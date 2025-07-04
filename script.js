52593f44a87db8b1a983ff2614989a08c2a365a1
ConorColwell97 <conormcolwell@gmail.com>
2025-07-04 02:19:06 +0100

    Implemented a square grid
    The user can input the number of squares they want
    to see on each side and then grid fills up evenly

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let numOfSquares = 16;
-addSquares(numOfSquares);
+const btn = document.querySelector('button');
+const container = document.querySelector('#container');
+
+btn.addEventListener('click', () => {
+    let numOfSquares = prompt('Enter squares per side');
+    container.innerHTML = null;
+
+    if(numOfSquares <= 0 || numOfSquares > 100) {
+        alert('Number is either too big or too small! Try again');
+    } else {
+        addSquares(numOfSquares**2);
+    }
+});
 
 function addSquares(numOfSquares) {
-    const container = document.querySelector('#container');
-
-    for(let i = 0; i < numOfSquares; i++) {
+    for (let i = 0; i < numOfSquares; i++) {
         const square = document.createElement('div');
-        square.classList.add('square');
+        const size = 800 / Math.sqrt(numOfSquares);
+        square.setAttribute(`style`, `width: ${size-2}px; height: ${size-2}px; border: 1px solid black;`);
         container.appendChild(square);
     }
 }
