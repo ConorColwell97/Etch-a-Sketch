68b06fdefd8198306b11193d683b8848041f4f99
ConorColwell97 <conormcolwell@gmail.com>
2025-07-04 14:13:41 +0100

    Added color change on hover to the squares
    Each square in the grid will now change to a
    random color upon hovering over them

--- script.js
+++ script.js
@@ -5,18 +5,31 @@ btn.addEventListener('click', () => {
     let numOfSquares = prompt('Enter squares per side');
     container.innerHTML = null;
 
-    if(numOfSquares <= 0 || numOfSquares > 100) {
+    if (numOfSquares <= 0 || numOfSquares > 100) {
         alert('Number is either too big or too small! Try again');
     } else {
-        addSquares(numOfSquares**2);
+        addSquares(numOfSquares ** 2);
     }
 });
+
 
 function addSquares(numOfSquares) {
     for (let i = 0; i < numOfSquares; i++) {
         const square = document.createElement('div');
+        square.classList.add('square');
+
         const size = 800 / Math.sqrt(numOfSquares);
-        square.setAttribute(`style`, `width: ${size-2}px; height: ${size-2}px; border: 1px solid black;`);
+        square.style.width = `${size - 2}px`;
+        square.style.height = `${size - 2}px`;
+
+        const val1 = parseInt(Math.random() * 255);
+        const val2 = parseInt(Math.random() * 255);
+        const val3 = parseInt(Math.random() * 255);
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = `rgb(${val1}, ${val2}, ${val3})`;
+        });
+
         container.appendChild(square);
     }
 }
