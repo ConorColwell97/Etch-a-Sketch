db1a6416cd4628fa9f9d2f87a24a829675f397f2
ConorColwell97 <conormcolwell@gmail.com>
2025-07-04 15:17:05 +0100

    Squares now darken until black when repeatedly hovered over
    The value of the rgb colors associated with each
    square reduces by 10% upon each hover, causing the
    squares to darken

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ btn.addEventListener('click', () => {
     let numOfSquares = prompt('Enter squares per side');
     container.innerHTML = null;
 
-    if (numOfSquares <= 0 || numOfSquares > 100) {
+    if ((numOfSquares <= 0 || numOfSquares > 100) && numOfSquares != null) {
         alert('Number is either too big or too small! Try again');
     } else {
         addSquares(numOfSquares ** 2);
@@ -17,17 +17,25 @@ function addSquares(numOfSquares) {
     for (let i = 0; i < numOfSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-
+        
+        // Calculate the size each square needs to be to fit into the grid
         const size = 800 / Math.sqrt(numOfSquares);
         square.style.width = `${size - 2}px`;
         square.style.height = `${size - 2}px`;
 
-        const val1 = parseInt(Math.random() * 255);
-        const val2 = parseInt(Math.random() * 255);
-        const val3 = parseInt(Math.random() * 255);
+        // Assign random rgb values
+        let val1 = parseInt(Math.random() * 255);
+        let val2 = parseInt(Math.random() * 255);
+        let val3 = parseInt(Math.random() * 255);
 
         square.addEventListener('mouseover', () => {
+            // Give the squares a random color
             square.style.backgroundColor = `rgb(${val1}, ${val2}, ${val3})`;
+
+            // Darken each square by around 10% upon each mouse hover until they are black
+            val1 = val1 > 0 ? (parseInt(val1 - ((10 / 100) * val1))) : 0;
+            val2 = val2 > 0 ? (parseInt(val2 - ((10 / 100) * val2))) : 0;
+            val3 = val3 > 0 ? (parseInt(val3 - ((10 / 100) * val3))) : 0;
         });
 
         container.appendChild(square);
